986c08a988430a18d66e8db4b9b2757646880eef
Rehana Sayyed <rehanasd555@gmail.com>
2024-10-06 16:43:59 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,18 +11,21 @@ export default function Main(){
             header="5.0 (6) USA"
             text1="Life lessons with Katie Zaferes"
             text2="From $136 / person"
+            soldOut={true}
             />
             <Card 
             img={star2}
             header="5.0 (30) USA"
             text1="Learn wedding photography"
             text2="From $125 / person"
+            soldOut={false}
             />
             <Card 
             img={star3}
             header="4.8 (2) USA"
             text1="Group Mountain Biking"
             text2="From $50 / person"
+            soldOut={false}
             />
         </main>
 
